e70433d4cbf2f8cd2e3b5b324ea8dbc39495b94a
Nora Priour-Petitbon <nora.pp@orange.fr>
2026-04-27 15:23:40 +0200

    initialisation flyway

---
+++ ceppa_back/src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    user_name  VARCHAR(250) NOT NULL,
+    email      VARCHAR(250) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ ceppa_back/src/main/resources/db/migration/V2__test_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (user_name, email, password)
+VALUES ( 'test_name', 'test_email@gmail.com', '1234');
