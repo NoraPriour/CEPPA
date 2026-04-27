6b81f42cf317e7d0f9c3a1788e55ef6b3346a072
Nora Priour-Petitbon <nora.pp@orange.fr>
2026-04-27 10:13:40 +0200

    Initialisation back

---
+++ ceppa_back/src/test/java/org/github/norapriour/ceppa_back/CeppaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.norapriour.ceppa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeppaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
