ff0e05ebe47e372eb437d7c9d9b7fd861d38eded
Nora Priour-Petitbon <nora.pp@orange.fr>
2026-04-29 18:25:37 +0200

    création function save sur ArticleRepository.java

--- ceppa_back/src/main/java/org/github/norapriour/ceppa_back/ArticleRepository.java
+++ ceppa_back/src/main/java/org/github/norapriour/ceppa_back/ArticleRepository.java
@@ -1,8 +1,11 @@
 package org.github.norapriour.ceppa_back;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,5 +19,20 @@ public class ArticleRepository {
 
     public List<Article> findAll() {
         return jdbc.query("SELECT a.id, a.titre, a.texte, COALESCE(u.user_name, 'Auteur inconnu') AS auteur FROM articles a LEFT JOIN users u ON a.auteur_id = u.id", (rs, rowNum) -> new Article(rs.getInt("id"), rs.getString("auteur"), rs.getString("titre"), rs.getString("texte")));
+    }
+
+    public Article save(Article article) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbc.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO articles (titre, texte, auteur_id) VALUES (?, ?, ?)", new String[]{"id"});
+            ps.setString(1, article.getTitre());
+            ps.setString(2, article.getTexte());
+            ps.setString(3, article.getAuteur());
+            return ps;
+        }, keyHolder);
+
+        int id = keyHolder.getKey().intValue();
+        return new Article(id, article.getAuteur(), article.getTitre(), article.getTexte());
     }
 }
